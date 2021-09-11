601aa26c3a97c11f2abe5aa2ae7201d65a865c25
keisuke <n.kei.0801.1997@gmail.com>
2021-09-11 10:42:16 +0900

    first-commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>current team:{{$route.query.title}}</h1>
+    <button @click="increase">count</button>
+    <h2>{{$store.state.count}}</h2>
+    <p>Last team name clicked:{{$route.query.title}}</p>
+    <router-link to="/">戻る</router-link>
   </div>
 </template>
+
+
+<script>
+export default {
+  props: ['data.name'],
+
+  methods:{
+    increase(){
+      this.$store.commit('incrementMutation');
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>チーム一覧</h1>
+      <router-link :to="{path:'/about', query: {title:data.name}}" v-for="(data, name) in teams" :key="name">
+        <p>{{data.name}}</p>
+      </router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      teams: [
+        {
+          id: "c36136ef-552f-4dc9-84b6-65ecb8574e81",
+          name: "Team_A",
+        },
+        {
+          id: "ed0459f5-716d-4ef4-99ba-5dc750dfe0d0",
+          name: "Team_B",
+        },
+        {
+          id: "60d0d5d5-8ce9-41d9-a65b-13a090c10ae1",
+          name: "Team_C",
+        },
+      ],
+    };
+
   }
-}
+};
 </script>
+
+<style scoped>
+div {
+  line-height: 30px;
+}
+</style>
+
